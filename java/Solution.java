0d3ddc321fb4aa8640a9b917ee0e6874ffe1d7fd
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-02-10 23:26:06 -0500

    js solution added

--- java/Solution.java
+++ java/Solution.java
@@ -2,7 +2,6 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m-1, j = n-1 ; 
         while( i >= 0 && j >= 0){
-            System.out.println(i+" "+ j);
             if(nums1[i] >= nums2[j]){
                 nums1[i+j+1] = nums1[i];
                 i--;
